50826b4773b134adf5489bf2954a7181cf2a21fd
Azarn <Alex3296@yandex.ru>
2016-03-15 12:49:42 +0300

    Seems working

--- cat/cat.c
+++ cat/cat.c
@@ -1,16 +1,32 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 
 int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Usage cat <input file>\n");
+        return -1;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        printf("Error while opening file!\n");
+        return errno;
+    }
+
     char buf[4096];
     size_t cnt;
-    while((cnt = read(0, buf, 4096)) > 0) {
+    while((cnt = read(fd, buf, 4096)) > 0) {
         size_t written = 0;
         while (cnt > 0) {
             written = write(1, &buf[written], cnt);
             cnt -= written;
         }
     }
+
+    close(fd);
     return 0;
 }
